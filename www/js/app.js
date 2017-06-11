5f2e3d8c81ec925a71c20930feb956f2907834cd
Federico Bouzas <federicobouzas@gmail.com>
2017-06-11 19:32:21 -0300

    Subida inicial

---
+++ www/js/app.js
@@ -0,0 +1,91 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('timbreo', ['ionic', 'ngCordova'])
+
+        .run(function ($ionicPlatform) {
+            $ionicPlatform.ready(function () {
+                if (window.cordova && window.cordova.plugins.Keyboard) {
+                    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                    // for form inputs)
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+                    // Don't remove this line unless you know what you are doing. It stops the viewport
+                    // from snapping when text inputs are focused. Ionic handles this internally for
+                    // a much nicer keyboard experience.
+                    cordova.plugins.Keyboard.disableScroll(true);
+                }
+                if (window.StatusBar) {
+                    StatusBar.styleDefault();
+                }
+            });
+        })
+
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                    .state('timbreo', {
+                        url: '/timbreo',
+                        templateUrl: 'templates/timbreo.html'
+                    })
+                    .state('login', {
+                        url: '/login',
+                        templateUrl: 'templates/login.html'
+                    });
+            $urlRouterProvider.otherwise('/login');
+        })
+
+        .factory('PouchDB', function () {
+            return PouchDB;
+        })
+
+        .controller('LoginController', function ($rootScope, $scope, $state) {
+            $scope.colores = ['Naranja', 'Azul', 'Rosa'];
+            $scope.comunas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+            $rootScope.user = {'email': 'federicobouzas@gmail.com', 'comuna': '5', 'color': 'Naranja', 'mapa': 22};
+            $scope.login = function () {
+                $state.go("timbreo");
+            };
+        })
+
+        .controller('TimbreoController', function ($rootScope, $scope, PouchDB, $ionicPopup, $timeout) {
+            var dbLocal = new PouchDB('timbreo');
+            PouchDB.replicate(dbLocal, 'http://127.0.0.1:5984/timbreo', {live: true, retry: true});
+            $scope.preguntas = {
+                1: {
+                    texto: 'Que te parece la gestion de Macri?',
+                    opciones: {"MB": "Muy Buena", "B": "Buena", "R": "Regular", "M": "Mala", "MM": "Muy Mala"}
+                },
+                2: {
+                    texto: 'Que te parece la gestion de HRL?',
+                    opciones: {"MB": "Muy Buena", "B": "Buena", "R": "Regular", "M": "Mala", "MM": "Muy Mala"}
+                },
+            };
+            var resetForm = function () {
+                $scope.formulario = {user: $rootScope.user, position: {}, respuestas: {}};
+            };
+            $scope.seleccionar = function (numero, valor) {
+                $scope.formulario.respuestas[numero] = valor;
+            };
+            $scope.guardar = function () {
+                var formulario = $scope.formulario;
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    formulario.position.latitude = position.coords.latitude;
+                    formulario.position.longitude = position.coords.longitude;
+                    dbLocal.post(formulario);
+                }, function (error) {
+                    console.log('code: ' + error.code + '\nmessage: ' + error.message + '\n');
+                });
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Formulario guardado!',
+                    template: '<ion-icon name="star"></ion-icon>',
+                    buttons: []
+                });
+                $timeout(function () {
+                    alertPopup.close();
+                }, 3000);
+                resetForm();
+            };
+            resetForm();
+        });
